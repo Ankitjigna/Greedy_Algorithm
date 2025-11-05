4fb0d0daee214a1ac000b124596cbbcf19fdac3a
Ankit Pal <114059115+Ankitjigna@users.noreply.github.com>
2025-11-06 01:08:53 +0530

    maximum activity selection for unsorted ( sorted on the basis of end time) using greeedy approach

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+System.out.println("max activities = " + maxAct);
+        // for(int i=0; i<ans.size(); i++){
+        // System.out.println("A" + ans.get(i)+" ");
+        // }
+        // System.out.println();
